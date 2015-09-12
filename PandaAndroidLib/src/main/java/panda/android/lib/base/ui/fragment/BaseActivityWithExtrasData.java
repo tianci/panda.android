372f9ae63ed9e52e7678e6870e2076119167b682
Panda <shitianci2012@163.com>
2015-09-12 11:55:31 +0800

    1、添加携带数据的Activity基类 2、增加liteorm类

---
+++ PandaAndroidLib/src/main/java/panda/android/lib/base/ui/fragment/BaseActivityWithExtrasData.java
@@ -0,0 +1,33 @@
+package panda.android.lib.base.ui.fragment;
+
+import android.os.Bundle;
+
+import panda.android.lib.base.model.BaseModel;
+import panda.android.lib.base.ui.BaseActivity;
+
+/**
+ * 作为需要解析额外数据的基类
+ *
+ * @param <T> 需要加载的主Fragment的类型
+ * @param <D> 需要加载的数据的类型
+ *
+ * Created by shitianci on 15/8/19.
+ */
+public abstract class BaseActivityWithExtrasData<T extends BaseFragment, D extends BaseModel> extends BaseActivity<T>{
+
+    public static final String ACTIVITY_EXTRA_DATA = "data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * 获取Activity的额外数据
+     * @param param
+     * @return
+     */
+    public D getExtrasData(Class<? extends D> param) {
+        return BaseModel.getGson().fromJson(getIntent().getStringExtra(ACTIVITY_EXTRA_DATA), param);
+    }
+}
