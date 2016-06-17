372eca9b3c0b3c092a4445ebfd48139943eb0111
YangYongLu <960611948@qq.com>
2016-06-17 13:01:36 +0800

    添加A-Z的排序

---
+++ PandaAndroidLib/src/main/java/panda/android/lib/base/ui/view/SideLetterBar.java
@@ -0,0 +1,143 @@
+package panda.android.lib.base.ui.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import panda.android.lib.R;
+
+
+public class SideLetterBar extends View {
+    private static final String TAG = SideLetterBar.class.getSimpleName();
+    private String[] b;         //引导显示的字符串    热门 A—Z
+    private int choose = -1;
+    private Paint paint = new Paint();
+    private boolean showBg = false;
+    private OnLetterChangedListener onLetterChangedListener;
+    private TextView overlay;
+
+    public SideLetterBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SideLetterBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SideLetterBar(Context context) {
+        super(context);
+    }
+
+
+    public void setNavTitle(String[] b) {
+        this.b = b;
+    }
+
+    /**
+     * 设置悬浮的textview
+     *
+     * @param overlay
+     */
+    public void setOverlay(TextView overlay) {
+        this.overlay = overlay;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (showBg) {
+            canvas.drawColor(Color.TRANSPARENT);
+        }
+
+        int height = getHeight();
+        int width = getWidth();
+        if (b == null) {
+            Log.e(TAG, "请设置 setNavTitle A—Z");
+        }
+        int singleHeight = height / b.length;
+        for (int i = 0; i < b.length; i++) {
+            paint.setTextSize(getResources().getDimension(R.dimen.text_size_720_1280_24));
+            paint.setColor(getResources().getColor(R.color.text_weak));
+            paint.setAntiAlias(true);
+            if (i == choose) {
+                paint.setColor(getResources().getColor(R.color.text_weak));
+//                paint.setFakeBoldText(true);  //加粗
+            }
+            float xPos = width / 2 - paint.measureText(b[i]) / 2;
+            float yPos = singleHeight * i + singleHeight;
+            canvas.drawText(b[i], xPos, yPos, paint);
+            paint.reset();
+        }
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        final int action = event.getAction();
+        final float y = event.getY();
+        final int oldChoose = choose;
+        final OnLetterChangedListener listener = onLetterChangedListener;
+        final int c = (int) (y / getHeight() * b.length);
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                showBg = true;
+                if (oldChoose != c && listener != null) {
+                    if (c >= 0 && c < b.length) {
+                        listener.onLetterChanged(b[c]);
+                        choose = c;
+                        invalidate();
+                        if (overlay != null) {
+                            overlay.setVisibility(VISIBLE);
+                            overlay.setText(b[c]);
+                        }
+                    }
+                }
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (oldChoose != c && listener != null) {
+                    if (c >= 0 && c < b.length) {
+                        listener.onLetterChanged(b[c]);
+                        choose = c;
+                        invalidate();
+                        if (overlay != null) {
+                            overlay.setVisibility(VISIBLE);
+                            overlay.setText(b[c]);
+                        }
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                showBg = false;
+                choose = -1;
+                invalidate();
+                if (overlay != null) {
+                    overlay.setVisibility(GONE);
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    public void setOnLetterChangedListener(OnLetterChangedListener onLetterChangedListener) {
+        this.onLetterChangedListener = onLetterChangedListener;
+    }
+
+    public interface OnLetterChangedListener {
+        void onLetterChanged(String letter);
+    }
+
+}
